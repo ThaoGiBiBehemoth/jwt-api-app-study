691f7d5b01a62250132df45474f29e84916f8ae4
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-03-08 11:10:45 +0700

    9th: add login action to authenticate user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
+    
     if @user.valid?
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }, status: :ok
+    else
+      render json: { error: 'Invalid username or password' }, status: :unprocessable_entity #422
+    end
+  end
+
+  def login
+    @user = User.find_by(username: user_params[:username])
+    if @user && @user.authenticate(user_params[:password])
       token = encode_token({ user_id: @user.id })
       render json: { user: @user, token: token }, status: :ok
     else
